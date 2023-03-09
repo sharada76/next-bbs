21b8156bc600ee827f6eacc208a832177b2d2ed2
sharada <sharada@haradashinyanoMacBook-Pro-2.local>
2023-03-10 00:36:17 +0900

    first commit

---
+++ backend/src/app/Models/Thread.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Response;
+
+class Thread extends Model
+{
+    use HasFactory, \Staudenmeir\EloquentEagerLimit\HasEagerLimit;
+
+    protected $fillable = [
+        'title',
+        'name',
+        'email',
+        'content',
+    ];
+
+    public function responses()
+    {
+        return $this->hasMany(Response::class);
+    }
+}
---
+++ backend/src/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ThreadController;
+use App\Http\Controllers\ResponseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/threads', [ThreadController::class, 'index']);
+Route::post('/threads', [ThreadController::class, 'store']);
+Route::get('/threads/{id}', [ThreadController::class, 'show']);
+
+Route::post('/threads/{threadId}/responses', [ResponseController::class, 'store']);
